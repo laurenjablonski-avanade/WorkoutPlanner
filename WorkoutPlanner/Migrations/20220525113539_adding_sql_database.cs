ee6e9f54fc8b7a07090621077a9173bfecd5507a
Lauren Jablonski <lauren.jablonski@avanade.com>
2022-06-01 15:26:43 +0100

    uploading final stages - with the front end stuff

---
+++ WorkoutPlanner/Migrations/20220525113539_adding_sql_database.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WorkoutPlanner.Migrations
+{
+    public partial class adding_sql_database : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
